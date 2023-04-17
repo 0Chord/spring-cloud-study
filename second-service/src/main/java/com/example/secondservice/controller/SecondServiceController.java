948671827e21a0c9d7e2915a5c02a99fbb35593e
0Chord <seaweed.0chord@gmail.com>
2023-04-17 15:23:02 +0900

    feat : 추가 api-gateway, filter

---
+++ second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -0,0 +1,25 @@
+package com.example.secondservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/second-service/")
+public class SecondServiceController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to the second-service";
+	}
+
+	@GetMapping("/message")
+	public String message(@RequestHeader("second-request") String header) {
+		log.info(header);
+		return "Hello World at Second-service";
+	}
+}
