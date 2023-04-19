7316f517a9b30fe1843d468cc6d9be1dbe0a16e9
0Chord <seaweed.0chord@gmail.com>
2023-04-19 14:01:03 +0900

    feat : 추가 LoggingFilter, EurekaServer 등록

--- second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -1,5 +1,9 @@
 package com.example.secondservice.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/second-service/")
 public class SecondServiceController {
+
+	Environment env;
+
+	@Autowired
+	public SecondServiceController(Environment env) {
+		this.env = env;
+	}
 
 	@GetMapping("/welcome")
 	public String welcome() {
@@ -24,7 +35,10 @@ public class SecondServiceController {
 	}
 
 	@GetMapping("/check")
-	public String check(){
-		return "Hi, there. This is a message from second-service";
+	public String check(HttpServletRequest request) {
+		log.info("Server Port={}", request.getServerPort());
+
+		return String.format("Hi, there. This is a message from second-service on Port %s",
+			env.getProperty("local.server.port"));
 	}
 }
