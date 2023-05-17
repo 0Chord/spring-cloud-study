87919c981af247486b6d073b51c7170f343bee8e
0Chord <seaweed.0chord@gmail.com>
2023-05-17 23:04:58 +0900

    feat : 추가 RestTemplate

--- ecommerce-project/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ ecommerce-project/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,11 +6,16 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.repository.UserEntity;
@@ -25,6 +30,8 @@ public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
+	private final RestTemplate restTemplate;
+	private final Environment env;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -47,8 +54,14 @@ public class UserServiceImpl implements UserService {
 			throw new UsernameNotFoundException("User not found");
 		}
 		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
-		List<ResponseOrder> orders = new ArrayList<>();
-		userDto.setOrders(orders);
+		// List<ResponseOrder> orders = new ArrayList<>();
+		String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+		ResponseEntity<List<ResponseOrder>> orderListResponse =
+			restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<ResponseOrder>>() {
+				});
+		List<ResponseOrder> orderList = orderListResponse.getBody();
+		userDto.setOrders(orderList);
 		return userDto;
 	}
 
