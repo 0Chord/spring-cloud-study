a931b7c685c734843359c2fd129263acfe4e4066
0Chord <seaweed.0chord@gmail.com>
2023-07-21 00:01:15 +0900

    feat : 추가 config-service Dockerfile

--- ecommerce-project/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ ecommerce-project/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -30,7 +30,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/actuator/**").permitAll();
 		http.authorizeRequests()
 			.antMatchers("/**")
-			.access("hasIpAddress('" + "172.30.1.232" + "')")
+			.access("hasIpAddress('" + "172.30.3.175" + "')")
 			.and()
 			.addFilter(getAuthenticationFilter());
 		http.headers().frameOptions().disable();
